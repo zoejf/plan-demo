ce7e613cbc7fbe44fbc1d36207438e6eea4c7370
zoejf <zoejfoss@gmail.com>
2015-11-19 14:18:29 -0800

    create index page

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 
+app.get('/', function(req, res) {
+	res.render('index');
+});
 
 app.listen(3000, function() {
 	console.log('I am listening');
